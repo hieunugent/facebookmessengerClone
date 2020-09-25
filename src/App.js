f1731ff519117e94d808866734896f2ce4709b91
hieunugent <hieunugent@gmail.com>
2020-09-25 01:23:10 -0700

    add the edit message to bottom up

--- src/App.js
+++ src/App.js
@@ -1,58 +1,71 @@
-import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import {  FormControl, Input } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import db from "./firebase";
 import Message from "./Message";
 import firebase from "firebase";
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
+import { IconButton } from "@material-ui/core";
+
 function App() {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([
-    // { username: "henry", text: "hello" },
-    // { username: "Hary", text: "hi there" },
+  
   ]);
   const [username, setUsername] = useState("");
   //listener
-  useEffect(()=> {
-     // run one when the app componet load 
-      db.collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot  => {
-        setMessages(snapshot.docs.map(doc => doc.data()))
-      })
-  },[])
+  useEffect(() => {
+    // run one when the app componet load
+    db.collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => ({id:doc.id, message: doc.data()})));
+      });
+  }, []);
 
   useEffect(() => {
     // run code here
     // if its blank insiude [] , this code runs ONce when the app component load
     //  const name = prompt('please enter your name');
     setUsername(prompt("please enter your name"));
-
   }, []); // conditions
 
   const sendMessage = (event) => {
     // all the logic to send the code here
     event.preventDefault();
 
-    db.collection('messages').add({
+    db.collection("messages").add({
       message: input,
-      username:username,
+      username: username,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    })
+    });
+    
 
+    
     // setMessages([...messages, {username:username, text:input}]);
     setInput("");
-  };
-
+  };;
+  console.log();
   return (
-    <div className="App">
-      <h1>Hello Programmer</h1>
+    <div className="app">
+      <img
+        src="https://facebookbrand.com/wp-content/uploads/2019/10/Messenger_Logo_Color_RGB.png?w=100&h=100"
+        alt=""
+      />
+      <h1>FaceBook Clone Messenger</h1>
       <h2>Welcome {username}</h2>
-      <form>
-        <FormControl>
-          <InputLabel>Enter a message</InputLabel>
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          {/* <InputLabel>Enter a message</InputLabel> */}
           <Input
+            className="app__formInput"
+            placeholder="Enter your message"
             value={input}
             onChange={(event) => setInput(event.target.value)}
           />
-          <Button
+          <IconButton
+            className="app__iconButton"
             disabled={!input}
             variant="contained"
             color="primary"
@@ -60,16 +73,21 @@ function App() {
             onClick={sendMessage}
           >
             {" "}
-            Send message
-          </Button>
+            <SendIcon />
+          </IconButton>
         </FormControl>
         {/* button */}
       </form>
 
       {/* message themselves */}
-      {messages.map((message) => (
-        <Message username={username} message={message} />
-      ))}
+
+      <FlipMove>
+        <div className="app__Message">
+          {messages.map(({ id, message }) => (
+            <Message key={id} username={username} message={message} />
+          ))}
+        </div>
+      </FlipMove>
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -1,20 +1,29 @@
 import { Card, CardContent, Typography } from "@material-ui/core";
-import React from "react";
+import React, { forwardRef } from "react";
+import moment from "moment";
+
 import "./Message.css";
-function Message({username, message}) {
-    // check userfname
-    const isUser = username === message.username;
+const Message =  forwardRef(({ username, message },ref) =>{
+  // check userfname
+  const isUser = (username === message.username);
   return (
-    <div className={`message ${isUser && "message__user"}`}>
+    <div ref={ref} className={`message ${isUser && "message__user"}`}>
       <Card className={isUser ? "message__userCard" : "message__guestCard"}>
         <CardContent>
           <Typography color="white" variant="h5" component="h2">
-            {message.username}: {message.message}
+            {!isUser && ` ${message.username || "Unknown User"}: `}
+            {message.message}
           </Typography>
         </CardContent>
       </Card>
+      <div className={isUser? "message__timeInfo":"message__timeInfo"}>
+        <small>
+          {" "}
+          {moment.unix(message.timestamp).format("MMMM Do, h:mma")}
+        </small>
+      </div>
     </div>
   );
-}
+})
 
 export default Message;
